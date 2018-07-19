b13fadb2bb275940a95a9915d0ad06421bb56ad0
Bashir Shafii <bashir.shafii@gmail.com>
2018-07-19 08:30:58 -0500

    added and updated content

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,7 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Mile Stone 1</title>
+</head>
+
+
+
+<body>
+	<h2>Purpose, Audience & Goal </h2>
+	<p><strong>Purpose: </strong>A professional portfolio to showcase my skill-set as a Web Developer who is
+		fluent in modern Web languages and frameworks. </p>
+	<p><strong>Audience:</strong> The primary audience for this site will be  small business owners and non-profit managers in
+		the Minneapolis/St Paul area whose organizations have little or no web presence. The secondary audience will be hiring managers,
+		technical recruiters and fellow Web Developers.</p>
+	<p><strong>Goal:</strong> The goal is to showcase my skill as a Web Developer and attract clients
+	who need websites for their small businesses and non-profits. </p>
+
+	<h2>Persona</h2>
+	<p><strong>Name:</strong>Yusuf Ali</p>
+	<p><strong>Age:</strong>48</p>
+	<p><strong>Profession:</strong> Founder of a small non-profit, retired business executive</p>
+	<p><strong>Technology:</strong>Ali is very comfortable with technology. He has access to both Android and Apple devices
+	. </p>
+	<p><strong>Attitude and Behavior:</strong>He understands the power of technology and would like to harness it to
+	take his new non-profit off the ground. He knows that it will be easier to market his non-profit and raise money 
+	if he has a great looking website. </p>
+	<p><strong>Needs and Frustrations:</strong>He has reached out to a few foundations and corporations seeking funding for this non-profit, 
+	. They always ask for his website so that they can read up on his mission and goals. He has no website. He has reached out to a few software 
+	development shops in Minneapolis but they are either too expensive or too busy working with big corporate clients. </p>
+	<p><strong>Goals:</strong>To have a simple static website with a contact form</p>
+	<p><strong>User Story:</strong>As a non-profit founder, I would like to create a website detailing our goals, mission and vision,
+	so that we can attract funding and volunteers. </p>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: bashirshafii
- * Date: 7/16/18
- * Time: 1:51 PM
- */
+
+
+
+?>
+</body>
+</html>
